e93d791925c53ffebc46c6e95ae3886b57dbc86c
xiangyujia <xiangyj_hzau@163.com>
2019-06-18 08:54:56 +0800

    day17 practice update rnaseq123

--- day17/RNAseqdata_analysis.R
+++ day17/RNAseqdata_analysis.R
@@ -24,10 +24,141 @@ suppressPackageStartupMessages({
 ## read data
 read.delim(file.path(".", files[1]), nrow=5)
 
-## Use readDEG read counts
+## Use readDEG read counts 将9个样本转为DGE对象
 x <- readDGE(file.path(".", files), columns=c(1,3))
 class(x)
 dim(x)
 
-## Organising sample information
+## Organising sample information 设定sample的名字
+samplenames <- substring(colnames(x), 12, nchar(colnames(x)))
+samplenames
+
+## 设置sample信息
+colnames(x) <- samplenames
+group <- as.factor(c("LP", "ML", "Basal", "Basal", "ML", "LP", 
+                     "Basal", "ML", "LP"))
+x$samples$group <- group
+lane <- as.factor(rep(c("L004","L006","L008"), c(3,4,2)))
+x$samples$lane <- lane
+x$samples
+
+## using the Mus.musculus package 
+## to retrieve associated gene symbols and chromosome information
+
+geneid <- rownames(x)
+genes <- select(Mus.musculus, keys=geneid, columns=c("SYMBOL", "TXCHROM"), 
+                keytype="ENTREZID")
+head(genes)
+## !duplicated replicate genes
+genes <- genes[!duplicated(genes$ENTREZID),]
+## add genes
+x$genes <- genes
+x
+
+## convert counts to CPM and log-CPM
+cpm <- cpm(x)
+lcpm <- cpm(x, log=TRUE)
+
+## Removing genes that are lowly expressed
+# 首先查看那些在所有样本中均为0的基因数目
+table(rowSums(x$counts==0)==9)
+# 
+keep.exprs <- rowSums(cpm>1)>=3
+x <- x[keep.exprs,, keep.lib.sizes=FALSE]
+dim(x)
+
+## 绘图展示过滤前后log-cpm值的分布情况
+library(RColorBrewer)
+nsamples <- ncol(x)
+col <- brewer.pal(nsamples, "Paired")
+par(mfrow=c(1,2))
+plot(density(lcpm[,1]), col=col[1], lwd=2, ylim=c(0,0.21), las=2, 
+     main="", xlab="")
+title(main="A. Raw data", xlab="Log-cpm")
+abline(v=0, lty=3)
+for (i in 2:nsamples){
+  den <- density(lcpm[,i])
+  lines(den$x, den$y, col=col[i], lwd=2)
+}
+legend("topright", samplenames, text.col=col, bty="n")
+lcpm <- cpm(x, log=TRUE)
+plot(density(lcpm[,1]), col=col[1], lwd=2, ylim=c(0,0.21), las=2, 
+     main="", xlab="")
+title(main="B. Filtered data", xlab="Log-cpm")
+abline(v=0, lty=3)
+for (i in 2:nsamples){
+  den <- density(lcpm[,i])
+  lines(den$x, den$y, col=col[i], lwd=2)
+}
+legend("topright", samplenames, text.col=col, bty="n")
+
+## 使用TMM标准化基因表达分布
+x <- calcNormFactors(x, method = "TMM")
+x$samples$norm.factors
+
+## 可视化准备
+# 可视化数据预处理
+x2 <- x
+x2$samples$norm.factors <- 1
+x2$counts[,1] <- ceiling(x2$counts[,1]*0.05)
+x2$counts[,2] <- x2$counts[,2]*5
+# 绘制箱线图
+par(mfrow=c(1,2))
+lcpm <- cpm(x2, log=TRUE)
+boxplot(lcpm, las=2, col=col, main="")
+title(main="A. Example: Unnormalised data",ylab="Log-cpm")
+x2 <- calcNormFactors(x2)  
+x2$samples$norm.factors
+#> [1] 0.05472223 6.13059440 1.22927355 1.17051887 1.21487709 1.05622968
+#> [7] 1.14587663 1.26129350 1.11702264
+lcpm <- cpm(x2, log=TRUE)
+boxplot(lcpm, las=2, col=col, main="")
+title(main="B. Example: Normalised data",ylab="Log-cpm")
+
+## 非监督聚类
+# 展示样本之间的差异性与相似性
+# 同一个处理的多个重复，没啥实验误差的话一般会聚在一起
+lcpm <- cpm(x, log=TRUE)
+par(mfrow=c(1,2))
+col.group <- group
+levels(col.group) <-  brewer.pal(nlevels(col.group), "Set1")
+col.group <- as.character(col.group)
+col.lane <- lane
+levels(col.lane) <-  brewer.pal(nlevels(col.lane), "Set2")
+col.lane <- as.character(col.lane)
+plotMDS(lcpm, labels=group, col=col.group)
+title(main="A. Sample groups")
+plotMDS(lcpm, labels=lane, col=col.lane, dim=c(3,4))
+title(main="B. Sequencing lanes")
+# Glimma 提供的绘MDS图函数
+glMDSPlot(lcpm, labels=paste(group, lane, sep="_"), 
+          groups=x$samples[,c(2,5)], launch=FALSE)
+
+
+## 差异表达分析
+# First,建立分组信息
+design <- model.matrix(~0+group+lane)
+colnames(design) <- gsub("group", "", colnames(design))
+design
+
+# Second,use makeContrasts function 建立比较信息
+contr.matrix <- makeContrasts(
+  BasalvsLP = Basal-LP, 
+  BasalvsML = Basal - ML, 
+  LPvsML = LP - ML, 
+  levels = colnames(design))
+contr.matrix
+
+# Third, Removing heteroscedascity from count data
+par(mfrow=c(1,2))
+v <- voom(x, design, plot=TRUE)
+v
+
+vfit <- lmFit(v, design)
+vfit <- contrasts.fit(vfit, contrasts=contr.matrix)
+efit <- eBayes(vfit)
+plotSA(efit, main="Final model: Mean-variance trend")
+
+# Fourth, Fitting linear models for comparisons of interest
+
 
